97554cd722f7748d55b42de0a330cdf0187acf2f
DanielNobbe <daniellnobbe@gmail.com>
2023-10-31 10:21:38 +0000

    added generic terms for prompt names

--- SegGPT/SegGPT_inference/infer_chain.py
+++ SegGPT/SegGPT_inference/infer_chain.py
@@ -12,6 +12,7 @@ from seggpt_inference import prepare_model
 from PIL import Image
 
 import time
+import yaml
 
 imagenet_mean = np.array([0.485, 0.456, 0.406])
 imagenet_std = np.array([0.229, 0.224, 0.225])
@@ -21,28 +22,14 @@ def main():
     input_image = 'input/images/IMG_0124.JPG'
     save_masks = True
 
-    area_prompts = {
-        'masks': [
-            "input/masks/IMG_0084_frame_mask.jpg",
-        "input/masks/IMG_0100_frame_mask.jpg"
-        ],
-        'images': [
-            "input/images/IMG_0113.JPG",
-            "input/images/IMG_0126.JPG"
-        ]
-    }
+    prompt_cfg_path = 'configs/prompt_config.yaml'
+    with open(prompt_cfg_path, 'r') as f:
+        prompt_cfg = yaml.safe_load(f)
+
+    roi_prompts = prompt_cfg['roi']
 
 
-    object_prompts = {
-        'masks': [
-            "input/masks/IMG_0113_capped_mask.jpg",
-            "input/masks/IMG_0126_capped_mask.jpg" 
-        ],
-        'images': [
-            "input/images/IMG_0113.JPG",
-            "input/images/IMG_0126.JPG"
-        ]
-    }
+    object_prompts = prompt_cfg['object']
 
     output_path = 'chain-output'
     os.makedirs(output_path, exist_ok=True)
@@ -54,21 +41,21 @@ def main():
     default_seg_type = "instance"  # TODO: Try semantic
     model = prepare_model(default_ckpt_path, default_model, default_seg_type).to(device)
 
-    # run inference to get area
-    prompt_images = area_prompts['images']
-    prompt_masks = area_prompts['masks']
-    area_output = os.path.join(output_path, 'area.png')
-    area_overlay_output = os.path.join(output_path, 'area_overlay.png')
-    area_mask = inference_image(model, device, input_image, prompt_images, prompt_masks, area_output, area_overlay_output, return_mask=True, upscale=False)
+    # run inference to get roi
+    prompt_images = roi_prompts['images']
+    prompt_masks = roi_prompts['masks']
+    roi_output = os.path.join(output_path, 'roi.png')
+    roi_overlay_output = os.path.join(output_path, 'roi_overlay.png')
+    roi_mask = inference_image(model, device, input_image, prompt_images, prompt_masks, roi_output, roi_overlay_output, return_mask=True, upscale=False)
 
-    # now convert area_mask to an image to see it
+    # now convert roi_mask to an image to see it
     # threshold mask
     threshold = 20
-    area_mask = area_mask.max(axis=-1)  # convert to greyscale
-    area_mask = area_mask > threshold
+    roi_mask = roi_mask.max(axis=-1)  # convert to greyscale
+    roi_mask = roi_mask > threshold
     if save_masks:
-        area_mask_img = Image.fromarray(255 * area_mask.astype(np.uint8), mode='L')
-        area_mask_img.save(os.path.join(output_path, 'area_mask.png'))
+        roi_mask_img = Image.fromarray(255 * roi_mask.astype(np.uint8), mode='L')
+        roi_mask_img.save(os.path.join(output_path, 'roi_mask.png'))
 
     # run inference to get objects
     prompt_images = object_prompts['images']
@@ -80,26 +67,26 @@ def main():
     # convert object_mask to an image to see it
     threshold = 20
     object_mask = object_mask.max(axis=-1)  # convert to greyscale
-    object_mask = (object_mask > threshold) & area_mask
+    object_mask = (object_mask > threshold) & roi_mask
     if save_masks:
         object_mask_img = Image.fromarray(255 * object_mask.astype(np.uint8), mode='L')
         object_mask_img.save(os.path.join(output_path, 'object_mask.png'))
 
 
-    # determine number of pixels that make up area
-    area_pixels = np.count_nonzero(area_mask)  # could also just do sum
-    print("Number of pixels for the area:", area_pixels)
+    # determine number of pixels that make up roi
+    roi_pixels = np.count_nonzero(roi_mask)  # could also just do sum
+    print("Number of pixels for the roi:", roi_pixels)
 
-    # determine number of pixels that make up object cells
+    # determine number of pixels that make up objects
     object_pixels = np.count_nonzero(object_mask)
     print("Number of pixels for objects:", object_pixels)
 
     # calculate percentage of objects
-    object_fraction = object_pixels / area_pixels
+    object_fraction = object_pixels / roi_pixels
 
     print("object fraction:", object_fraction)
 
-    # TODO: Only use object cells segmented in area area for improved
+    # TODO: Only use objects segmented in roi for improved
     # accuracy --> could we add this as an extra prompt?
     # TODO: Don't scale back up to original resolution, not necessary for
     # fraction
